a64369472684caeae6a7801f9e87a77183c76b7d
Atakan <158854874+Atakanlift@users.noreply.github.com>
2024-08-08 18:19:17 +0300

    last

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,7 +1,8 @@
-import HomeSide from "@/e-page/HomeSide";
-import LeftBar from "@/navbar/LeftBar";
-import TopBar from "@/navbar/TopBar";
+
 import Image from "next/image";
+import HomeSide from "./e-page/HomeSide";
+import TopBar from "./navbar/TopBar";
+import LeftBar from "./navbar/LeftBar";
 
 
 export default function Home() {
